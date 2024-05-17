64a554a7fccde447c1c327e729017fc7e1613342
luc4s-git <lucassantos4332@hotmail.com>
2024-05-16 23:33:55 -0300

    add margin to the logo to separate the theme btn from it

--- components/dashboard/sidebar/SidebarHeader.tsx
+++ components/dashboard/sidebar/SidebarHeader.tsx
@@ -4,9 +4,11 @@ import ThemeToggle from './ThemeToggle';
 
 export default function SidebarHeader() {
   return (
-    <div className="flex items-center mb-4 gap-4 px-4">
-      <SiOpenaigym className="w-12 h-12 text-primary" />
-      <h2 className="text-2xl font-bold text-primary">{metadata.title}</h2>
+    <div className="flex items-center mb-4 gap-2 px-4">
+      <SiOpenaigym className="w-10 h-10 text-primary" />
+      <h2 className="text-2xl font-bold text-primary mr-auto">
+        {metadata.title}
+      </h2>
       <ThemeToggle />
     </div>
   );
